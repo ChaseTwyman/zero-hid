1e326260340d333720cf07b45a962e691e05454d
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-10-21 02:47:22 +0000

    removed + - and **

--- tests/random_keys.py
+++ tests/random_keys.py
@@ -1,15 +1,11 @@
 import random
 from zero_hid import Keyboard, KeyCodes
-number = 0
+number = 2
 with Keyboard() as k:
     for i in range(1, 11):
         num1 = random.randint(1,100)
-        num2 = random.randint(1,10000)
-        num3 = random.randint(1,20)
-        num5 = random.randint(1,2)
-        number += num1
+        num2 = random.randint(1,100)
+        number *= num1
         number -= num2
-        number *= num3
-        number = number ** (1/num5) if number >= 0 else -number ** (1/num5)
         k.type(str(number))
         k.press([], KeyCodes.KEY_ENTER)
