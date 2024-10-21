061147eda42d085f7b2c80551a5e1c122825776e
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-10-21 01:56:41 +0000

    made number game in random_keys

--- tests/random_keys.py
+++ tests/random_keys.py
@@ -1,6 +1,19 @@
 import random
 from zero_hid import Keyboard, KeyCodes
+number = 0
 with Keyboard() as k:
     for i in range(1, 1001):
-        k.type(str(i))
-        k.press([], KeyCodes.KEY_ENTER)
+        num1 = random.randint(1,100)
+        num2 = random.randint(1,100)
+        num3 = random.randint(1,20)
+        num4 = random.randint(1,20)
+        num5 = random.randint(1,2)
+        num6 = random.randint(1,2)
+        number += num1
+        number -= num2
+        number *= num3
+        number /= num4
+        number **= num5
+        number **= 1/num6
+        k.type(number)
+        k.press([], KeyCodes.KEY_TAB)
