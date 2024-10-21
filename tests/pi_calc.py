cf96313a35fe9bb9db3e5ee5eed37a66a0112066
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-10-21 02:36:52 +0000

    edit

--- tests/pi_calc.py
+++ tests/pi_calc.py
@@ -1,8 +1,9 @@
 from zero_hid import Keyboard, KeyCodes
+import math
 with Keyboard() as k:
     pi = 0
     for i in range(1001):
         sign = (-1) ** i
         pi += sign * 4 / (2 * i + 1)
-        k.type(str(pi))
-        k.press([], KeyCodes.KEY_TAB)
+        k.type(str(math.pi - pi))
+        k.press([], KeyCodes.KEY_ENTER)
