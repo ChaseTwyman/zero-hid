a204740908f1d9b8457196324642ad6476ec9d17
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-10-21 02:11:19 +0000

    edit

--- tests/random_keys.py
+++ tests/random_keys.py
@@ -4,7 +4,7 @@ number = 0
 with Keyboard() as k:
     for i in range(1, 1001):
         num1 = random.randint(1,100)
-        num2 = random.randint(1,100)
+        num2 = random.randint(1,10000)
         num3 = random.randint(1,20)
         num4 = random.randint(1,20)
         num5 = random.randint(1,2)
@@ -12,8 +12,6 @@ with Keyboard() as k:
         number += num1
         number -= num2
         number *= num3
-        number /= num4
         number **= num5
-        number **= 1/num6
         k.type(str(number))
         k.press([], KeyCodes.KEY_TAB)
