18a2d0428f44b43911ba0a6a191e071280fbeb71
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-10-21 02:49:13 +0000

    added time delay

--- tests/random_keys.py
+++ tests/random_keys.py
@@ -1,6 +1,8 @@
 import random
 from zero_hid import Keyboard, KeyCodes
+from time import sleep
 number = 2
+sleep(5)
 with Keyboard() as k:
     for i in range(1, 11):
         num1 = random.randint(1,100)
