3e195b9c357898252b2f1fe7ddf400ae8fa455b1
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-10-21 02:50:14 +0000

    edit

--- tests/random_keys.py
+++ tests/random_keys.py
@@ -4,10 +4,10 @@ from time import sleep
 number = 2
 sleep(5)
 with Keyboard() as k:
-    for i in range(1, 11):
+    for i in range(1, 1001):
         num1 = random.randint(1,100)
         num2 = random.randint(1,100)
         number *= num1
-        number -= num2
+        number /= num2
         k.type(str(number))
         k.press([], KeyCodes.KEY_ENTER)
