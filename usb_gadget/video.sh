5ac6e6c3f677c7dd9b1e4116e12c3ebbb59d4d08
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-11-05 23:18:19 +0000

    fixed pipeline?

--- usb_gadget/video.sh
+++ usb_gadget/video.sh
@@ -1,2 +1,2 @@
-USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6) | awk '{print $NF}'
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6) | awk '{print $28}'
 v4l2-ctl --set-edid=file="$USER_HOME/TC358743-Driver/1080p25edid"
