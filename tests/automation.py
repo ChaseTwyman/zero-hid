138ea346f632b404ccbd375d0b6e489036b77c28
GameWire9 <chasetwyman3@gmail.com>
2024-11-24 01:44:29 -0500

    can check for windows updates, need to make sure that scaling is correct (create another macro that addresses this by using OCR to get to display scaling settings) and need to make sure that each computer the RPZ2W is connected to is on Duplicate Display

--- tests/automation.py
+++ tests/automation.py
@@ -15,11 +15,17 @@ import subprocess
     
     # Press Button
 # Check for Window's Updates
+constants = {
+    "repeat_num" : 5
+}
 OS = {
     "version": ""
 }
 Checklist = {
-    "Windows Update" : False,
+    "Windows Update" : {
+        "Check for Updates" : False,
+        "Install Updates" : False
+    },
     "Install Apps" : False,
     "Clean Temporary Files" : False,
     "Optimize Drive" : False,
@@ -57,10 +63,10 @@ with Keyboard() as k, Mouse(absolute=False) as rel_mouse, Mouse(absolute=True) a
 
     # Open Settings
     k.press([KeyCodes.MOD_LEFT_GUI], KeyCodes.KEY_I)
-    sleep(1)
-    k.press([KeyCodes.MOD_LEFT_GUI], KeyCodes.KEY_UP)
-
+    
     if OS["version"] == "Windows 10":
+        sleep(1)
+        k.press([KeyCodes.MOD_LEFT_GUI], KeyCodes.KEY_UP)
         with OCR(True, {"width":148, "height":21, "x":878, "y":658}, "update_path") as update_path:
             if update_path.lower() == "update & security\n":
                 with Move_Mouse(74+888, 10+668) as mouse_move: {}
@@ -72,21 +78,52 @@ with Keyboard() as k, Mouse(absolute=False) as rel_mouse, Mouse(absolute=True) a
                         if "quality" == found_text[0][i].lower():
                             download_button = 1
                             break
-                    for i in range(len(found_text[0]), 0):
-                        if "download" == found_text[0][i].lower():
-                            if download_button == 0:
-                                with Move_Mouse(found_text[1][i][2], found_text[1][i][3]) as mouse_move: {}
-                                break
-                            else:
-                                download_button -= 1
-"""
-
-
+                    with OCR(True, {"width":592-445, "height":250-230, "x":445, "y":230}, "check_for_update_button") as check_for_update_button:
+                        if "check for updates\n" == check_for_update_button.lower():
+                            with Move_Mouse(445+(592-445)/2, 230+(250-230)/2) as mouse_move: {}
+                            sleep(1)
+                            rel_mouse.left_click()
+                            Checklist["Windows Update"]["Check for Update"] = True
+                        else:
+                            for i in range(len(found_text[0]), 0):
+                                    if "download" == found_text[0][i].lower():
+                                        if download_button == 0:
+                                            with Move_Mouse(found_text[1][i][2], found_text[1][i][3]) as mouse_move: {}
+                                            break
+                                        else:
+                                            download_button -= 1
+                                        if download_button < 0:
+                                            print("some fatal error occured")
 
     if OS["version"] == "Windows 11":
-        with OCR(True, 238-80, 819-794, 80, 794, "update_path") as update_path:
-            if update_path.lower() == "windows update"
-
+        with OCR(True, {"width":238-80, "height":819-794, "x":80, "y":794}, "update_path") as update_path:
+            complete = False
+            for i in range(0, constants["repeat_num"]):
+                if update_path.lower() == "windows update\n":
+                    with Move_Mouse(80+(238-80)/2, 794+(819-794)/2) as mouse_move: {}
+                    sleep(1)
+                    rel_mouse.left_click()
+                    complete = False
+                    for i in range(0, constants["repeat_num"]):
+                        with OCR(True, {"width":1862-1697, "height":249-224, "x":1697, "y":224}, "check_for_update_button") as button:
+                            if button.lower() == "check for updates\n":
+                                with Move_Mouse(1697+(1862-1697)/2, 224+(249-224)/2) as mouse_move: {}
+                                sleep(1)
+                                rel_mouse.left_click()
+                                complete = True
+                                Checklist["Windows Update"]["Check for Updates"] = True
+                                break
+                        if i == constants["repeat_num"] and not complete:
+                            print("error clicking windows button")
+                    complete = True
+                    break
+                else:
+                    complete = False
+                if i == constants["repeat_num"] and not complete:
+                    print("error occured when checking for windows update path")
+                    
+                
+"""
         abs_mouse.move(30500,7000)
     elif OS["version"] == "Windows 10":
         abs_mouse.move(8800,7100)
