f66963e2e138ab92ddc702df03693a05d4d7771d
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-12-18 15:32:44 +0000

    edit

--- tests/test2.py
+++ tests/test2.py
@@ -1,4 +1,4 @@
 from zero_hid import Mouse, Keyboard, KeyCodes
 with Keyboard() as k, Mouse() as rel_mouse:
-    k.press([KeyCodes.MOD_LEFT_ALT, KeyCodes.MOD_LEFT_SHIFT, KeyCodes.KEY_TAB])
-    k.press([], KeyCodes.MOD_LEFT_GUI)
+    # k.press([KeyCodes.MOD_LEFT_ALT, KeyCodes.MOD_LEFT_SHIFT, KeyCodes.KEY_TAB])
+    k.press([KeyCodes.MOD_LEFT_GUI])
