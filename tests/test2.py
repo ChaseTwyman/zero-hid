a00a7b49544ae7f10b89fec8dfa83b8324b034e3
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-12-18 15:49:45 +0000

    edit

--- tests/test2.py
+++ tests/test2.py
@@ -2,8 +2,7 @@ from zero_hid import Mouse, Keyboard, KeyCodes
 from time import sleep
 with Keyboard() as k, Mouse() as rel_mouse:
     sleep(2)
-    k.press([KeyCodes.MOD_LEFT_ALT], 0, False)
+    k.press([KeyCodes.MOD_LEFT_ALT, KeyCodes.MOD_LEFT_SHIFT], KeyCodes.KEY_TAB, False)
     sleep(1)
     k.press([], KeyCodes.KEY_TAB)
     sleep(0.5)
-    # k.press([KeyCodes.MOD_LEFT_ALT, KeyCodes.MOD_LEFT_SHIFT], KeyCodes.KEY_TAB)
