266287edc41b32892a7805608b29c20e0533c2fb
OG-Wire <44178776+GameWire9@users.noreply.github.com>
2024-12-18 15:48:33 +0000

    test

--- tests/test2.py
+++ tests/test2.py
@@ -4,6 +4,6 @@ with Keyboard() as k, Mouse() as rel_mouse:
     sleep(2)
     k.press([KeyCodes.MOD_LEFT_ALT], 0, False)
     sleep(1)
-    # k.press([KeyCodes.MOD_LEFT_SHIFT], KeyCodes.KEY_TAB)
+    k.press([], KeyCodes.KEY_TAB)
     sleep(0.5)
     # k.press([KeyCodes.MOD_LEFT_ALT, KeyCodes.MOD_LEFT_SHIFT], KeyCodes.KEY_TAB)
